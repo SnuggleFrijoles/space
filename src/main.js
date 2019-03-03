2afe1b88bd3df82ff7131c8c3f5efbce3140dad8
kruegz <krueger.zach@gmail.com>
2019-03-03 12:45:11 -0600

    First update, basic gravitational physics and moon graphics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'phaser';
-import BootScene from './scenes/BootScene';
+// import BootScene from './scenes/BootScene';
 import GameScene from './scenes/GameScene';
-import TitleScene from './scenes/TitleScene';
+// import TitleScene from './scenes/TitleScene';
 
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
@@ -9,20 +9,19 @@ const config = {
     pixelArt: true,
     roundPixels: true,
     parent: 'content',
-    width: 400,
-    height: 240,
+    width: 800,
+    height: 600,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {
-                y: 800
-            },
+            // gravity: {
+            //     y: 800
+            // },
+            // debug: true
             debug: false
         }
     },
     scene: [
-        BootScene,
-        TitleScene,
         GameScene
     ]
 };
---
+++ src/sprites/GravitationalBody.js
@@ -0,0 +1,63 @@
+
+
+export default class GravitationalBody extends Phaser.GameObjects.Sprite {
+
+    constructor(scene, x, y, xVel, yVel, mass, key) {
+
+        super(scene, x, y, key);
+        scene.physics.world.enable(this);
+        scene.add.existing(this);
+
+        this.mass = mass; // Mass is equivalent to area
+        this.radius = Math.sqrt(mass / Math.PI);
+
+        // this.setOrigin(0, 0);
+        this.setScale(this.radius / 512);
+        // this.setGravity(10);
+        // this.setVelocity(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
+        this.body.setVelocity(xVel, yVel);
+
+        this.body.setAngularAcceleration(10);
+
+        this.body.setMaxVelocity(1000, 1000);
+
+        this.body.setMaxAcceleration = function(maxX, maxY) {
+            this.maxAccelerationX = maxX;
+            this.maxAccelerationY = maxY;
+        }
+
+        // this.body.setMaxAcceleration(1, 1);
+
+        this.body._setAcceleration = function(x, y) {
+            this.setAcceleration(x, y);
+            if (this.maxAccelerationX && x > this.maxAccelerationX) {
+                this.setAccelerationX(this.maxAccelerationX);
+            }
+            if (this.maxAccelerationy && y > this.maxAccelerationY) {
+                this.setAccelerationY(this.maxAccelerationY);
+            }
+        }
+
+        this.body.setBounce(1);
+        // this.body.setCollideWorldBounds(true);
+        // this.body.setSize(512, 512);
+        this.body.setSize(512,512);
+        this.body.setOffset(0, 0);
+
+        ;
+        // this.anims.play('stand');
+
+        // this.on('animationcomplete', () => {
+        //     if (this.anims.currentAnim.key === 'grow' || this.anims.currentAnim.key === 'shrink') {
+        //         this.scene.physics.world.resume();
+        //     }
+        // }, this);
+    }
+
+    update(keys, time, delta) {
+
+    }
+
+
+
+}
